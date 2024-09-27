060de660ef88ea4c75d9137b593e7302cc3702ab
augusthottie <myaugusthottie@gmail.com>
2024-09-27 15:08:05 +0200

    👷Add pgdump module

---
+++ src/pgbackup/pgdump.py
@@ -0,0 +1,9 @@
+import sys
+import subprocess
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
